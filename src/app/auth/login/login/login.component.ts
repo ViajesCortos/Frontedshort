7ede72b1b898840e9d7812045a07c4b5f0b06bff
Santiago Liscano <santiagoliscanovargas@gmail.com>
2025-04-22 14:31:37 -0500

    fix:fix it before merge

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -3,33 +3,20 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-<<<<<<< HEAD
-import { AuthService } from '../../../Services/Auth.service';
-import { Router, RouterLink, RouterLinkWithHref } from '@angular/router';
-import { User } from '../../../Interfaces/User.model';
-=======
 import { AuthService } from '../../../Services/auth.service';
 import { Router, RouterLink, RouterLinkWithHref, RouterModule } from '@angular/router';
 import { User } from '../../../Interfaces/User.model';
 import { Auth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from '@angular/fire/auth';
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
 
 @Component({
   selector: 'app-login',
   standalone: true,
-<<<<<<< HEAD
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,RouterLinkWithHref,RouterLink],
-=======
   imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,RouterLinkWithHref,RouterLink,RouterModule],
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
-<<<<<<< HEAD
-=======
   auth: Auth = inject(Auth);
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
 authService = inject (AuthService);
 router = inject (Router);
 
@@ -42,33 +29,21 @@ OnSubmit(){
   if (this.forms.valid) {
     this.authService.logIn(this.forms.value as User)
     .then(r =>{
-<<<<<<< HEAD
-      this.router.navigate(['./dashboard']);
-=======
       console.log('usuario autenticado:',r.user);
       localStorage.setItem('token', JSON.stringify(r.user)); // Guarda el token
       this.router.navigate(['/dashboard']); 
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
     })
     .catch(error => console.log(error)
     )
   }
 }
 OnclickGoogle(){
-<<<<<<< HEAD
-  this.authService.logInGoogle()
-  .then(r => {
-    this.router.navigate(['./dashboard'])
-  }) 
-  .catch(error => console.log(error))
-=======
   const provider = new GoogleAuthProvider();
   signInWithPopup(this.auth, provider).then(result => {
     console.log('Usuario autenticado:', result.user);
     localStorage.setItem('token', JSON.stringify(result.user)); // Guarda el token
     this.router.navigate(['/dashboard']);  
   }).catch(error => console.error('Error al iniciar sesión:', error));
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
 
 }
 goToRegister() {
--- src/app/auth/register/register/register.component.ts
+++ src/app/auth/register/register/register.component.ts
@@ -1,9 +1,5 @@
 import { Component, inject } from '@angular/core';
-<<<<<<< HEAD
-import { AuthService } from '../../../Services/Auth.service'; 
-=======
-import { AuthService } from '../../../Services/auth.service'; 
->>>>>>> 18fe514 (feat:Mapa interactivo con marcadores test)
+import { AuthService } from "../../../Services/auth.service";  
 import { Validators, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { User } from '../../../Interfaces/User.model'; 
